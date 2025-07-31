ee428e7c0c6c58f43f293f7fe1493683020b5275
Jhon Castaneda Vergara <2909287@tcs.com>
2025-07-31 05:24:02 -0500

    Implementando varios enpoints

--- PythonAvanzado.py/map.py
+++ PythonAvanzado.py/map.py
@@ -1,3 +1,6 @@
+import pyautogui as pg
+import time
+
 
 serie_1 =range(1, 11)
 
@@ -6,4 +9,16 @@ listas =list(serie_1)
 lista_numeros =list(map(lambda numero : numero**2, listas))
 
 print(lista_numeros)
+
+# Tiempo entre clics (en segundos)
+intervalo = 50 
+
+
+time.sleep(3)  
+
+while True:
+    pg.click()  
+    print("Clic realizado.")
+    time.sleep(intervalo)
+
 
