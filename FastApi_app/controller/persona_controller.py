6900c352dcc8e39b0bd03f29515a62866a5a2e0d
Jhon Castaneda Vergara <2909287@tcs.com>
2025-08-04 21:53:33 -0500

    Aprendiendo appi

--- FastApi_app/controller/persona_controller.py
+++ FastApi_app/controller/persona_controller.py
@@ -1,8 +1,18 @@
 from model.persona_model import Personas
+from fastapi import HTTPException
 
 
 lista_de_personas=[]
 
 async def create_personal(data: Personas):
-    lista_de_personas.append(data)
-    return data
+
+    try:
+
+        lista_de_personas.append(data)
+        return data
+
+    except:
+        raise HTTPException(
+        status_code=404,
+        detail="No funciona está mierda"
+    )
--- FastApi_app/routes/persona_routes.py
+++ FastApi_app/routes/persona_routes.py
@@ -2,8 +2,9 @@
 from fastapi import APIRouter
 from controller.persona_controller import create_personal
 from model.persona_model import Personas
+from typing import Annotated
 
-router = APIRouter()
+router = APIRouter(prefix="/Hola", tags=["hola"])
 
 @router.post("/persona")
 async def crear_persona(data: Personas):
